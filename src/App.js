2f70fe7963d17128fbdeafbb344bb2da4638d12e
Jeevakumar-A <Jeevajeeva40348@gmail.com>
2025-10-25 21:46:39 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+        <p className='text-3xl font-bold text-green-600 m-800px'>
+          Hello jeeva this is react...
+        </p>
+    </div>
+  );
+}
+
+export default App;
